07de884cfc6ab48cf5d3e2390a519d12238c6e89
Mike Rogers <mikerogers@Mikes-MacBook-Pro.local>
2012-11-23 10:41:44 +0000

    mkaing bricks 3d better

--- httpdocs/js/objects/bricks.js
+++ httpdocs/js/objects/bricks.js
@@ -16,10 +16,25 @@ Brick.prototype.draw = function(){
  	this.ctx.fillStyle = this.ctx.createPattern(patterns[this.pattern], 'repeat');
  	this.ctx.strokeStyle = '#000';
  	this.ctx.fillRect(this.location.x, this.location.y, this.size.w, this.size.h);
- 	this.ctx.strokeRect(this.location.x, this.location.y, this.size.w, this.size.h);
+ 	//this.ctx.strokeRect(this.location.x, this.location.y, this.size.w, this.size.h);
  	
+ 	// Add the 3d bit in.
  	this.ctx3d.fillStyle = '#d6d6d6';
  	this.ctx3d.strokeStyle = '#000';
- 	this.ctx3d.fillRect(this.location.x + 10, this.location.y - 20, this.size.w, this.size.h);
- 	this.ctx3d.strokeRect(this.location.x + 10, this.location.y - 20, this.size.w, this.size.h);
+ 	this.ctx3d.beginPath();
+ 	
+	this.ctx3d.moveTo(this.location.x ,this.location.y);
+	this.ctx3d.lineTo(this.location.x + 10,this.location.y - 20); // /
+	this.ctx3d.lineTo(this.location.x + this.size.w + 10,this.location.y - 20); // _
+	this.ctx3d.lineTo(this.location.x + this.size.w,this.location.y); // /
+	
+	this.ctx3d.moveTo(this.location.x + this.size.w + 10,this.location.y - 19);
+	this.ctx3d.lineTo(this.location.x + this.size.w + 10,this.location.y  + this.size.h - 20);
+	this.ctx3d.lineTo(this.location.x + this.size.w,this.location.y  + this.size.h);
+	this.ctx3d.lineTo(this.location.x + this.size.w,this.location.y + 1);
+	
+	this.ctx3d.stroke();
+	this.ctx3d.fill();
+ 	//this.ctx3d.fillRect(this.location.x + 10, this.location.y - 20, this.size.w, this.size.h);
+ 	//this.ctx3d.strokeRect(this.location.x + 10, this.location.y - 20, this.size.w, this.size.h);
 };
